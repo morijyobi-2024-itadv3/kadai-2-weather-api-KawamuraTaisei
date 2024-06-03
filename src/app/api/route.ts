6b1c9f99c9784489ed3193606338f54de0e24139
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2024-06-03 12:46:00 +0900

    岩手県内陸の天気を返すapi

---
+++ src/app/api/route.ts
@@ -0,0 +1,19 @@
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const pref = searchParams.get("pref");
+  const area = searchParams.get("area");
+
+  console.log(pref, area);
+
+  const res = await fetch(
+    `https://www.jma.go.jp/bosai/forecast/data/forecast/030000.json`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const weather = await res.json();
+
+  return Response.json(weather);
+}
