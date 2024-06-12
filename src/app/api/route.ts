381bfa4f9105a5f43a9ad288de087faa31ce0257
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2024-06-12 11:39:06 +0900

    入力されるパラメータが正しくない場合のエラー処理を追加

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,6 +5,10 @@ export async function GET(request: Request) {
 
   console.log(pref, area);
 
+  if (pref !== "岩手県" || area !== "内陸") {
+    return new Response("Invalid parameter", { status: 500 });
+  }
+
   const res = await fetch(
     `https://www.jma.go.jp/bosai/forecast/data/forecast/030000.json`,
     {
