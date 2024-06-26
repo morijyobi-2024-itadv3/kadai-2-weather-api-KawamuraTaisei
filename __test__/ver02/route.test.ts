995a3ce689176a12239f94078a180a46ccd418da
orenouserNamehaKawamura1 <ta.kawamura.sys22@morijyobi.ac.jp>
2024-06-26 12:06:29 +0900

    レスポンスのテストコードの追加と型ファイルの作成

--- __test__/ver02/route.test.ts
+++ __test__/ver02/route.test.ts
@@ -1,70 +1,90 @@
 // api/route.tsのテストファイル
-const API_URL = 'http://localhost:3000/api'
+import { type TypeResponse } from "@/app/api/type";
+const API_URL = 'http://localhost:3000/api';
 
 describe("APiへのリクエスト", () => {
-describe("パラメーターが正常に設定されている", () => {
-  const pref = encodeURIComponent("岩手県");
-  const area = encodeURIComponent("内陸");
-  it("HTTP STATUSが200である", async () => {
-    const response = await fetch(
-      `http://localhost:3000/api?pref=${pref}&area=${area}`
-    );
+  describe("パラメーターが正常に設定されている", () => {
+    const pref = encodeURIComponent("岩手県");
+    const area = encodeURIComponent("内陸");
+    it("HTTP STATUSが200である", async () => {
+      const response = await fetch(
+        `http://localhost:3000/api?pref=${pref}&area=${area}`
+      );
 
-    expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
+    });
   });
-});
 
-describe("パラメーターのprefが間違っている", () => {
-  const pref = encodeURIComponent("青森県");
-  const area = encodeURIComponent("内陸");
-  it("HTTP STATUSが400である", async () => {
-    const response = await fetch(
-      `http://localhost:3000/api?pref=${pref}&area=${area}`
-    );
+  describe("パラメーターのprefが間違っている", () => {
+    const pref = encodeURIComponent("青森県");
+    const area = encodeURIComponent("内陸");
+    it("HTTP STATUSが400である", async () => {
+      const response = await fetch(
+        `http://localhost:3000/api?pref=${pref}&area=${area}`
+      );
 
-    expect(response.status).toBe(400);
+      expect(response.status).toBe(400);
+    });
   });
-});
 
-describe("パラメーターのareaが間違っている", () => {
-  const pref = encodeURIComponent("岩手県");
-  const area = encodeURIComponent("沿岸");
-  it("HTTP STATUSが400である", async () => {
-    const response = await fetch(
-      `http://localhost:3000/api?pref=${pref}&area=${area}`
-    );
+  describe("パラメーターのareaが間違っている", () => {
+    const pref = encodeURIComponent("岩手県");
+    const area = encodeURIComponent("沿岸");
+    it("HTTP STATUSが400である", async () => {
+      const response = await fetch(
+        `http://localhost:3000/api?pref=${pref}&area=${area}`
+      );
 
-    expect(response.status).toBe(400);
+      expect(response.status).toBe(400);
+    });
   });
-});
 
 
-describe("両方のパラメーターが間違っている", () => {
-  const pref = encodeURIComponent("秋田県");
-  const area = encodeURIComponent("沿岸");
-  it("HTTP STATUSが400である", async () => {
-    const response = await fetch(
-      `http://localhost:3000/api?pref=${pref}&area=${area}`
-    );
+  describe("両方のパラメーターが間違っている", () => {
+    const pref = encodeURIComponent("秋田県");
+    const area = encodeURIComponent("沿岸");
+    it("HTTP STATUSが400である", async () => {
+      const response = await fetch(
+        `http://localhost:3000/api?pref=${pref}&area=${area}`
+      );
 
-    expect(response.status).toBe(400);
+      expect(response.status).toBe(400);
+    });
   });
-});
 
-describe("GETメソッド以外でアクセスされた", () => {
-  const pref = '岩手県'
-  const area = '内陸'
-  const methods = ['POST', 'PUT', 'DELETE', 'PATCH']
+  describe("GETメソッド以外でアクセスされた", () => {
+    const pref = '岩手県';
+    const area = '内陸';
+    const methods = ['POST', 'PUT', 'DELETE', 'PATCH'];
   
-  methods.forEach(async (method) => {
-    it(`HTTP STATUSが405である`, async () => {
-      const response = await fetch(`http://localhost:3000/api?pref=${pref}&area=${area}`, {
-        method: method,
-      });
+    methods.forEach(async (method) => {
+      it(`HTTP STATUSが405である`, async () => {
+        const response = await fetch(`http://localhost:3000/api?pref=${pref}&area=${area}`, {
+          method: method,
+        });
 
-      expect(response.status).toBe(405);
+        expect(response.status).toBe(405);
+      });
     });
   });
 });
+  
+describe('APIのレスポンス', () => {
+  describe('パラメーターが正しい場合', () => {
+    const pref = '岩手県';
+    const area = '内陸';
+    let response:Response;
+    let data:TypeResponse;
+
+    // テスト実行前にAPIにリクエストを送信し、レスポンスを取得する
+    beforeAll(async () => {
+      response = await fetch(`${API_URL}?pref=${pref}&area=${area}`);
+      data = await response.json();
+    });
+    describe('レスポンスヘッダ', () => {
+      it('レスポンスヘッダは application/json である', async () => {
+        expect(response.headers.get('Content-Type')).toBe('application/json');
+      });
+    });
+  });
 });
-   
--- jest.config.ts
+++ jest.config.ts
@@ -78,19 +78,21 @@ const config: Config = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+     '^@/(.*)$': '<rootDir>/src/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -126,9 +128,9 @@ const config: Config = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "<rootDir>"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -166,7 +168,9 @@ const config: Config = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
+  testRegex: [
+    '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  ],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
@@ -175,8 +179,9 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
-
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
